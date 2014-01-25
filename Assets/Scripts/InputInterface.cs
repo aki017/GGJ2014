8950f0d1068beb690367ea59497f2131bf410951
aki017 <aki@aki017.info>
2014-01-25 15:20:50 +0900

    InputInterface

---
+++ Assets/Scripts/InputInterface.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System;
+
+public abstract class InputInterface{
+	private List<Action> ups = new List<Action>();
+	private List<Action> downs = new List<Action>();
+	private List<Action> lefts = new List<Action>();
+	private List<Action> rights = new List<Action>();
+	private List<Action> bullets = new List<Action>();
+
+	void Init(){
+	}
+
+	public void RegistorUp(Action action){
+		ups.Add(action);
+	}
+	public void RegistorDown(Action action){
+		downs.Add(action);
+	}
+    public void RegistorLeft(Action action){
+		lefts.Add(action);
+	}
+	public void RegistorRight(Action action){
+		rights.Add(action);
+	}
+	public void RegistorBullet(Action action){
+		bullets.Add(action);
+	}
+
+	private void fireall(IList<Action> l){
+		foreach(var a in l){
+			a();
+		}
+	}
+
+	public void FireUp(){ fireall (ups); }
+	public void FireDown(){ fireall (downs); }
+	public void FireLeft(){ fireall (lefts); }
+	public void FireRight(){ fireall (rights); }
+}
---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class KeyboardInput : MonoBehaviour {
+	private static Dictionary<KeyCode, Action> inputs;
+
+	void Awake() {
+		i = new KeyBoardInterface();
+		inputs = new Dictionary<KeyCode, Action>(){};
+		inputs.Add (KeyCode.W, i.FireUp);
+		inputs.Add (KeyCode.A, i.FireLeft);
+		inputs.Add (KeyCode.S, i.FireDown);
+		inputs.Add (KeyCode.D, i.FireRight);
+	}
+
+	public static InputInterface i;
+	void Update () {
+		foreach(var k in inputs){
+			if(Input.GetKeyDown(k.Key)) {
+				k.Value();
+			}
+		}
+	}
+}
