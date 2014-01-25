1d5ffbcfe13d3ff2d535144fdcd166fa94236adb
aki017 <aki@aki017.info>
2014-01-25 18:10:00 +0900

    Lifebar

---
+++ Assets/LifeBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeBar : MonoBehaviour {
+	private float life = 0.0f;
+	public float Life { 
+		set{
+			life = value;
+			if(life > 100) life = 100;
+			transform.localScale = new Vector3(0.4f*life/100,0.4f, 1f);
+		}
+	}
+	void Update(){
+		Life = life + Random.value;
+	}
+}
